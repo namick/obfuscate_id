a08fa11e63b96e81f3e9089324ab3f798b0d03bb
Nathan Amick <github@nathanamick.com>
2012-02-01 05:32:48 +0000

    added methods to update find and to_param and add them to active record - specs pass

--- lib/obfuscate_id.rb
+++ lib/obfuscate_id.rb
@@ -1,2 +1,37 @@
 module ObfuscateId
+
+  def obfuscate_id
+    extend ClassMethods 
+    include InstanceMethods
+  end
+
+  def self.hide(id)
+    id.to_i + 100
+  end
+
+  def self.show(id)
+    id.to_i - 100
+  end
+
+  module ClassMethods
+    def find(*args)
+      if has_obfuscated_id?
+        args[0] = ObfuscateId.show(args[0])
+      end
+      super(*args)
+    end
+
+    def has_obfuscated_id?
+      true
+    end
+  end
+
+  module InstanceMethods
+    def to_param
+      ObfuscateId.hide(self.id)
+    end
+
+  end
 end
+
+ActiveRecord::Base.extend ObfuscateId
