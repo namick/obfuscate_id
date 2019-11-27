86f4f075b62f9599f4f34b8575f1c0a084800429
Basem Ezzat <basem.taha@optomatica.com>
2019-11-27 17:04:57 +0200

    produces new version, makes it compatible with rails 5

--- spec/dummy/config/application.rb
+++ spec/dummy/config/application.rb
@@ -22,6 +22,5 @@ module Dummy
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
   end
 end
